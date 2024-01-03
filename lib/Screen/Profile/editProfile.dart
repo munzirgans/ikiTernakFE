973bb9ef5c1a9d2bdec0f0028794e4c875541403
fiqrihermansyah <muhammadfikrihermansyah5@gmail.com>
2024-01-04 04:59:24 +0700

    Update Fiqri

--- lib/Screen/Profile/editProfile.dart
+++ lib/Screen/Profile/editProfile.dart
@@ -38,8 +38,7 @@ class _EditProfileState extends State<EditProfile> {
   TextEditingController dobController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   DateTime? selectedDate;
-  String avatarImageUrl =
-      "https://via.placeholder.com/90x90"; // Default avatar image URL
+  String avatarImageAsset = "assets/images/avatar.png";
 
   @override
   Widget build(BuildContext context) {
@@ -84,7 +83,7 @@ class _EditProfileState extends State<EditProfile> {
                   height: 90,
                   decoration: ShapeDecoration(
                     image: DecorationImage(
-                      image: NetworkImage(avatarImageUrl),
+                      image: AssetImage(avatarImageAsset),
                       fit: BoxFit.fill,
                     ),
                     shape: RoundedRectangleBorder(
@@ -109,21 +108,6 @@ class _EditProfileState extends State<EditProfile> {
                   ),
                 ),
               ),
-              // Positioned(
-              //   left: 167,
-              //   top: 253,
-              //   child: Text(
-              //     'Edit Avatar',
-              //     style: TextStyle(
-              //       color: Color(0xFF1BA0E2),
-              //       fontSize: 10,
-              //       fontFamily: 'Poppins',
-              //       fontWeight: FontWeight.w500,
-              //       decoration: TextDecoration.underline,
-              //       height: 0,
-              //     ),
-              //   ),
-              // ),
               Positioned(
                 left: 34,
                 top: 297,
